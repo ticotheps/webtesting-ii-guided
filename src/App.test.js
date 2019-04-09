6fe47c3e1fdc9f029af4cac2a8ec7eafa61b9b65
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-09 13:21:20 -0400

    Creates a 'describe()' statement to encapsulate the test cases inside of App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -5,23 +5,30 @@ import 'jest-dom/extend-expect';
 
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
-
-it('renders successfully', () => {
-  render(<App />);
-});
-
-it('renders Hello World', () => {
-  // const helpers = render(<App />); 
-  // the render method returns an object that gives you methods to help you find things in the DOM.
-  // console.log('helpers', helpers);
-
-  const { getByText, queryByText, debug } = render(<App />); 
+describe('<App />', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
   
-  // getByText(/hello World/i);
-  expect(queryByText(/hello world/i)).not.toBeNull();
+  it('renders successfully', () => {
+    render(<App />);
+  });
+  
+  it('renders Hello World', () => {
+    // const helpers = render(<App />); 
+    // the render method returns an object that gives you methods to help you find things in the DOM.
+    // console.log('helpers', helpers);
+  
+    const { getByText, queryByText, debug } = render(<App />); 
+    
+    // getByText(/hello World/i);
+    expect(queryByText(/hello world/i)).not.toBeNull();
+  });
+  
+  // The DIFFERENCE between 'getByText' and 'queryByText':
+  // 'getByText' can be used to check the presence of an element on the DOM and throws an error if the test fails.
+  // 'queryByText' can be used to do the same thing, but returns null if the test fails.
 });
+
